08e0866469a046f816e27a66dba14870c22c597f
azakhary <azakhary@rockbitegames.com>
2019-10-27 22:27:45 +0400

    adding copyright notice to all java file as well as AUTHORS and CONTRIBUTORS files

--- editor/src/com/rockbite/tools/talos/TalosLauncher.java
+++ editor/src/com/rockbite/tools/talos/TalosLauncher.java
@@ -1,9 +1,23 @@
+/*******************************************************************************
+ * Copyright 2019 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.rockbite.tools.talos;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglFrame;
-import com.rockbite.tools.talos.TalosMain;
 
 import java.awt.dnd.DropTarget;
 
