4546a8e0945e786d59c1529f4444cd462614a8de
Avetis Zakharyan <avetiszakharyan@Avetiss-MacBook-Pro.local>
2022-07-25 23:20:11 +0400

    wip on tween thing

--- editor/src/com/talosvfx/talos/editor/addons/scene/apps/tween/nodes/DelayNode.java
+++ editor/src/com/talosvfx/talos/editor/addons/scene/apps/tween/nodes/DelayNode.java
@@ -1,4 +1,11 @@
 package com.talosvfx.talos.editor.addons.scene.apps.tween.nodes;
 
 public class DelayNode extends AbstractGenericTweenNode {
+
+    @Override
+    protected void onSignalReceived(String command, Object[] payload) {
+
+        System.out.println("YATAAAAA I got signalz");
+
+    }
 }
--- editor/src/com/talosvfx/talos/editor/addons/scene/apps/tween/nodes/TweenNode.java
+++ editor/src/com/talosvfx/talos/editor/addons/scene/apps/tween/nodes/TweenNode.java
@@ -3,6 +3,7 @@ package com.talosvfx.talos.editor.addons.scene.apps.tween.nodes;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ObjectSet;
 import com.badlogic.gdx.utils.XmlReader;
 import com.talosvfx.talos.editor.addons.scene.apps.tween.TweenStage;
 import com.talosvfx.talos.editor.nodes.widgets.ButtonWidget;
@@ -19,11 +20,28 @@ public class TweenNode extends AbstractTweenNode {
     public void constructNode(XmlReader.Element module) {
         super.constructNode(module);
 
-        // add some additional widget logic here, if ay
+        ButtonWidget playButton = getButton("playButton");
+
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                String target = (String) (getWidget("target").getValue());
+                Object[] payload = new Object[1];
+                payload[0] = target;
+                sendSignal("startSignal", "execute", payload);
+                super.clicked(event, x, y);
+            }
+        });
+
     }
 
     public String getTweenTitle() {
         TextValueWidget titleText = (TextValueWidget) getWidget("title");
         return titleText.getValue();
+    }
+
+    @Override
+    protected void onSignalReceived(String command, Object[] payload) {
+
     }
 }
