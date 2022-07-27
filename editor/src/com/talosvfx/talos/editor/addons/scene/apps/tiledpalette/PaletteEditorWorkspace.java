da7ae1c6ec9b8c1258bb09050a8de37183e5c09b
Tom Wojciechowski <tomwojciechowski@asidik.com>
2022-07-27 16:38:27 +0400

    palette render

--- editor/src/com/talosvfx/talos/editor/addons/scene/apps/tiledpalette/PaletteEditorWorkspace.java
+++ editor/src/com/talosvfx/talos/editor/addons/scene/apps/tiledpalette/PaletteEditorWorkspace.java
@@ -1,19 +1,34 @@
 package com.talosvfx.talos.editor.addons.scene.apps.tiledpalette;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ObjectMap;
+import com.kotcrab.vis.ui.FocusManager;
+import com.talosvfx.talos.TalosMain;
 import com.talosvfx.talos.editor.addons.scene.SceneEditorWorkspace;
 import com.talosvfx.talos.editor.addons.scene.assets.GameAsset;
+import com.talosvfx.talos.editor.addons.scene.events.GameObjectSelectionChanged;
 import com.talosvfx.talos.editor.addons.scene.logic.Scene;
 import com.talosvfx.talos.editor.addons.scene.logic.TilePaletteData;
 import com.talosvfx.talos.editor.addons.scene.logic.components.MapComponent;
 import com.talosvfx.talos.editor.addons.scene.maps.TalosLayer;
+import com.talosvfx.talos.editor.addons.scene.widgets.gizmos.Gizmo;
+import com.talosvfx.talos.editor.notifications.Notifications;
 import com.talosvfx.talos.editor.utils.GridDrawer;
 import com.talosvfx.talos.editor.widgets.ui.ViewportWidget;
 
 import java.util.UUID;
 import java.util.function.Supplier;
+
+import static com.talosvfx.talos.editor.addons.scene.SceneEditorWorkspace.ctrlPressed;
 
 public class PaletteEditorWorkspace extends ViewportWidget {
     GameAsset<TilePaletteData> paletteData;
@@ -21,6 +36,9 @@ public class PaletteEditorWorkspace extends ViewportWidget {
 
     private GridDrawer gridDrawer;
     private SceneEditorWorkspace.GridProperties gridProperties;
+
+    private Image selectionRect;
+
 
     public PaletteEditorWorkspace(GameAsset<TilePaletteData> paletteData) {
         super();
@@ -50,6 +68,95 @@ public class PaletteEditorWorkspace extends ViewportWidget {
 
         gridDrawer = new GridDrawer(this, camera, gridProperties);
         gridDrawer.highlightCursorHover = true;
+
+        selectionRect = new Image(TalosMain.Instance().getSkin().getDrawable("orange_row"));
+        selectionRect.setSize(0, 0);
+        selectionRect.setVisible(false);
+        addActor(selectionRect);
+
+        addListener(new InputListener() {
+
+            // selection stuff
+            Vector2 startPos = new Vector2();
+            Vector2 vec = new Vector2();
+            Rectangle rectangle = new Rectangle();
+            boolean upWillClear = true;
+
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+
+                upWillClear = true;
+
+
+                if(button == 2 || ctrlPressed()) {
+                    selectionRect.setVisible(true);
+                    selectionRect.setSize(0, 0);
+                    startPos.set(x, y);
+
+                    getStage().setKeyboardFocus(PaletteEditorWorkspace.this);
+
+                    event.handle();
+
+
+                    return true;
+                }
+                return super.touchDown(event, x, y, pointer, button);
+            }
+
+            @Override
+            public void touchDragged (InputEvent event, float x, float y, int pointer) {
+                super.touchDragged(event, x, y, pointer);
+
+                if(selectionRect.isVisible()) {
+                    vec.set(x, y);
+                    vec.sub(startPos);
+                    if(vec.x < 0) {
+                        rectangle.setX(x);
+                    } else {
+                        rectangle.setX(startPos.x);
+                    }
+                    if(vec.y < 0) {
+                        rectangle.setY(y);
+                    } else {
+                        rectangle.setY(startPos.y);
+                    }
+                    rectangle.setWidth(Math.abs(vec.x));
+                    rectangle.setHeight(Math.abs(vec.y));
+
+                    selectionRect.setPosition(rectangle.x, rectangle.y);
+                    selectionRect.setSize(rectangle.getWidth(), rectangle.getHeight());
+
+                    event.handle();
+                }
+
+            }
+
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+
+                if(selectionRect.isVisible()) {
+                    upWillClear = false;
+                    //selectByRect(rectangle); todo
+//                    Notifications.fireEvent(Notifications.obtainEvent(GameObjectSelectionChanged.class).set(selection)); //todo
+                } else if(upWillClear) {
+                    FocusManager.resetFocus(getStage());
+//                    clearSelection(); todo
+//                    Notifications.fireEvent(Notifications.obtainEvent(GameObjectSelectionChanged.class).set(selection));
+                } else {
+                    if(!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+                        // deselect all others, if they are selected
+//                        if(deselectOthers(selectedGameObject)) {/ /todo
+//                            Notifications.fireEvent(Notifications.obtainEvent(GameObjectSelectionChanged.class).set(selection));
+//                        }
+                    }
+                }
+
+
+                selectionRect.setVisible(false);
+
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
     }
 
     @Override
@@ -75,6 +182,8 @@ public class PaletteEditorWorkspace extends ViewportWidget {
 
         batch.begin();
     }
+
+
 
     @Override
     public void act(float delta) {
