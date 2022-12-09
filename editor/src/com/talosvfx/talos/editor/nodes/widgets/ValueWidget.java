0767929b09e7dc2ec095391bc1392bf86c2edb54
Tom Wojciechowski <tomwojciechowski@asidik.com>
2022-12-09 17:12:57 +0400

    fix value widget edit mode

--- editor/src/com/talosvfx/talos/editor/nodes/widgets/ValueWidget.java
+++ editor/src/com/talosvfx/talos/editor/nodes/widgets/ValueWidget.java
@@ -119,7 +119,10 @@ public class ValueWidget extends AbstractWidget<Float> {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 dragged = false;
                 lastPos = x;
+
                 event.stop();
+
+                super.touchDown(event, x, y, pointer, button);
                 return true;
             }
 
