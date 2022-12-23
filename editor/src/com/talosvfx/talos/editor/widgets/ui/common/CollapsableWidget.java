487ba5caae614a8ac9098e56396d73cbdac36520
DaevaStep <davitstepanyan99@gmail.com>
2022-12-23 14:32:05 +0400

    added collapsable widgets

---
+++ editor/src/com/talosvfx/talos/editor/widgets/ui/common/CollapsableWidget.java
@@ -0,0 +1,55 @@
+package com.talosvfx.talos.editor.widgets.ui.common;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.talosvfx.talos.editor.project2.SharedResources;
+
+public class CollapsableWidget extends Table {
+    private final Table topSegment;
+    private final Table content;
+    private final Cell contentCell;
+
+    private boolean isCollapsed = true;
+
+    public CollapsableWidget () {
+        pad(5, 10, 5, 8).top();
+        setBackground(ColorLibrary.obtainBackground(ColorLibrary.SHAPE_SQUIRCLE_2, ColorLibrary.BackgroundColor.DARK_GRAY));
+
+        topSegment = constructTopSegment();
+        content = constructContent();
+
+        add(topSegment).growX();
+        row();
+        contentCell = add().grow();
+    }
+
+    public Table constructTopSegment () {
+        final Image collapseButton = new Image(SharedResources.skin.getDrawable("white"));
+        final Label widgetLabel = new Label("Display", SharedResources.skin, "small");
+
+        collapseButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                isCollapsed = !isCollapsed;
+                contentCell.setActor(isCollapsed ? null : content);
+            }
+        });
+
+        final Table topSegment = new Table();
+        topSegment.defaults().space(6);
+        topSegment.add(collapseButton).size(10);
+        topSegment.add(widgetLabel).expand().left();
+        return topSegment;
+    }
+
+    public Table constructContent () {
+        final Table content = new Table();
+        content.add().height(300);
+        return content;
+    }
+}
