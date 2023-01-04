5bfe7143e0c1a5cf0e257270ac658207bdda39b6
SenPie <smbo.voskanyan2002@gmail.com>
2023-01-04 14:56:19 +0400

    LayoutGrid edges to hilight and change cursor

--- editor/src/com/talosvfx/talos/editor/layouts/LayoutResizeWidget.java
+++ editor/src/com/talosvfx/talos/editor/layouts/LayoutResizeWidget.java
@@ -1,24 +1,47 @@
 package com.talosvfx.talos.editor.layouts;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.talosvfx.talos.editor.utils.CursorUtil;
 
 public class LayoutResizeWidget extends Table {
 
 	private final Skin skin;
 	private final LayoutGrid layoutGrid;
 
+
+	private LayoutItem responder;
+	private boolean entered;
+
 	public LayoutResizeWidget (Skin skin, LayoutGrid layoutGrid, LayoutItem responder) {
 		this.skin = skin;
 		this.layoutGrid = layoutGrid;
+		this.responder = responder;
 
 		setBackground(skin.newDrawable("white", Color.valueOf("333333ff")));
 
 		ClickListener listener = new ClickListener() {
+
+			@Override
+			public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+				entered = true;
+
+				setBackground(skin.newDrawable("white", Color.valueOf("666666ff")));
+				super.enter(event, x, y, pointer, fromActor);
+			}
+
+			@Override
+			public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+				entered = false;
+
+				setBackground(skin.newDrawable("white", Color.valueOf("333333ff")));
+				super.exit(event, x, y, pointer, toActor);
+			}
 
 			@Override
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
@@ -36,5 +59,18 @@ public class LayoutResizeWidget extends Table {
 		addListener(listener);
 
 		setTouchable(Touchable.enabled);
+	}
+
+
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+		if (entered) {
+			if (responder instanceof LayoutColumn) {
+				CursorUtil.setDynamicModeCursor(CursorUtil.CursorType.MOVE_VERTICALLY);
+			} else if (responder instanceof LayoutRow) {
+				CursorUtil.setDynamicModeCursor(CursorUtil.CursorType.MOVE_HORIZONTALLY);
+			}
+		}
 	}
 }
