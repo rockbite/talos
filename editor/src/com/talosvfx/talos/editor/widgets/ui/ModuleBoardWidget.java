a611e17a4ab8dc00e95c71dac754911eb226d1d1
Tom Wojciechowski <tomwojciechowski@asidik.com>
2023-01-05 11:34:36 +0000

    fix unfocus in talos vfx

--- editor/src/com/talosvfx/talos/editor/widgets/ui/ModuleBoardWidget.java
+++ editor/src/com/talosvfx/talos/editor/widgets/ui/ModuleBoardWidget.java
@@ -292,6 +292,7 @@ public class ModuleBoardWidget extends WidgetGroup {
 
                 if (!event.isHandled()) {
                     clearSelection();
+                    stage.unfocusAll();
                     return false;
                 }
 
