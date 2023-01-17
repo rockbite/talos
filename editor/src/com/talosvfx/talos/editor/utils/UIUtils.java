926b19bdc3880b352df1caf1bad77251f9c694d5
vuvkar <vuvkar@hotmail.co.uk>
2023-01-17 15:37:49 +0400

    little change

--- editor/src/com/talosvfx/talos/editor/utils/UIUtils.java
+++ editor/src/com/talosvfx/talos/editor/utils/UIUtils.java
@@ -1,7 +1,9 @@
 package com.talosvfx.talos.editor.utils;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -84,6 +86,13 @@ public class UIUtils {
 
 
     public static void registerFonts (Skin skin) {
+        FreeTypeFontGenerator freeTypeFontGenerator = new FreeTypeFontGenerator(Gdx.files.local("skin/VisOpenSans.ttf"));
+        for (int i = 4; i < 75; i++) {
+            FreeTypeFontGenerator.FreeTypeFontParameter freeTypeFontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+            freeTypeFontParameter.size = i;
+            BitmapFont bitmapFont = freeTypeFontGenerator.generateFont(freeTypeFontParameter);
+            orderedFontMap.put(i, bitmapFont);
+        }
 //        for (RegisteredFont value : RegisteredFont.values()) {
 //            Array<BitmapFont> bitmapFonts = new Array<>();
 //            BitmapFont font = skin.getFont(value.fontName);
@@ -101,18 +110,18 @@ public class UIUtils {
 //            registeredFonts.put(value, bitmapFonts);
 //        }
 
-        orderedFontMap.put(15, skin.getFont("default-font"));
-        orderedFontMap.put(12, skin.getFont("small-font"));
-        orderedFontMap.put(14, skin.getFont("generated-font-14"));
-        orderedFontMap.put(13, skin.getFont("generated-font-13"));
-        orderedFontMap.put(11, skin.getFont("generated-font-11"));
-        orderedFontMap.put(10, skin.getFont("generated-font-10"));
-        orderedFontMap.put(9, skin.getFont("generated-font-9"));
-        orderedFontMap.put(8, skin.getFont("generated-font-8"));
-        orderedFontMap.put(7, skin.getFont("generated-font-7"));
-        orderedFontMap.put(6, skin.getFont("generated-font-6"));
-        orderedFontMap.put(5, skin.getFont("generated-font-5"));
-        orderedFontMap.put(4, skin.getFont("generated-font-4"));
+//        orderedFontMap.put(15, skin.getFont("default-font"));
+//        orderedFontMap.put(12, skin.getFont("small-font"));
+//        orderedFontMap.put(14, skin.getFont("generated-font-14"));
+//        orderedFontMap.put(13, skin.getFont("generated-font-13"));
+//        orderedFontMap.put(11, skin.getFont("generated-font-11"));
+//        orderedFontMap.put(10, skin.getFont("generated-font-10"));
+//        orderedFontMap.put(9, skin.getFont("generated-font-9"));
+//        orderedFontMap.put(8, skin.getFont("generated-font-8"));
+//        orderedFontMap.put(7, skin.getFont("generated-font-7"));
+//        orderedFontMap.put(6, skin.getFont("generated-font-6"));
+//        orderedFontMap.put(5, skin.getFont("generated-font-5"));
+//        orderedFontMap.put(4, skin.getFont("generated-font-4"));
 
         for (BitmapFont value : orderedFontMap.values()) {
             value.setUseIntegerPositions(true);
