797f34a376842df5859469350a1da61e72c8dab2
vuvkar <vuvkar@hotmail.co.uk>
2023-01-23 17:38:12 +0400

    updated pr

--- editor/src/com/talosvfx/talos/editor/wrappers/FromToParticlePointDataGeneratorModuleWrapper.java
+++ editor/src/com/talosvfx/talos/editor/wrappers/FromToParticlePointDataGeneratorModuleWrapper.java
@@ -23,13 +23,11 @@ import com.talosvfx.talos.editor.notifications.Notifications;
 import com.talosvfx.talos.editor.notifications.events.deprecatedparticles.RegisterDragPoints;
 import com.talosvfx.talos.editor.notifications.events.deprecatedparticles.UnRegisterDragPoints;
 import com.talosvfx.talos.editor.widgets.ui.DragPoint;
-import com.talosvfx.talos.editor.widgets.ui.PreviewWidget;
-import com.talosvfx.talos.runtime.Slot;
-import com.talosvfx.talos.runtime.modules.AbstractModule;
-import com.talosvfx.talos.runtime.modules.FromToParticlePointDataGeneratorModule;
-import com.talosvfx.talos.runtime.modules.ParticlePointDataGeneratorModule;
-import com.talosvfx.talos.runtime.modules.StaticValueModule;
-import com.talosvfx.talos.runtime.modules.Vector2Module;
+import com.talosvfx.talos.runtime.vfx.Slot;
+import com.talosvfx.talos.runtime.vfx.modules.AbstractModule;
+import com.talosvfx.talos.runtime.vfx.modules.FromToParticlePointDataGeneratorModule;
+import com.talosvfx.talos.runtime.vfx.modules.ParticlePointDataGeneratorModule;
+import com.talosvfx.talos.runtime.vfx.modules.StaticValueModule;
 import com.talosvfx.talos.editor.widgets.ui.common.zoomWidgets.TextFieldWithZoom;
 
 
