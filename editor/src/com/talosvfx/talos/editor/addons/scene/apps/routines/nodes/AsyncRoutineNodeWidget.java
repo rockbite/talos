78214175cabeff214672209db4d0e6860beef992
SenPie <smbo.voskanyan2002@gmail.com>
2023-01-31 16:13:22 +0400

    better lines with shape render, takes into account min max values to not draw out of widget boundaires and changes line thickness based on zoom

--- editor/src/com/talosvfx/talos/editor/addons/scene/apps/routines/nodes/AsyncRoutineNodeWidget.java
+++ editor/src/com/talosvfx/talos/editor/addons/scene/apps/routines/nodes/AsyncRoutineNodeWidget.java
@@ -332,13 +332,12 @@ public class AsyncRoutineNodeWidget extends AbstractRoutineNodeWidget {
                     onRunStart();
                 }
 
-                timelineWidget.clearMap();
                 microNodeView.clearMap();
 
                 float maxDuration = (1 - states.first().alpha) * states.first().getDuration();
                 for (AsyncRoutineNodeState<?> state : states) {
                     Object target = state.getTarget();
-                    timelineWidget.setProgress(target, state.alpha);
+                    timelineWidget.setProgress(state.alpha);
                     microNodeView.setProgress(target, state.alpha);
 
                     if(maxDuration < (1 - state.alpha) * state.getDuration() && state.alpha > 0) {
@@ -348,7 +347,6 @@ public class AsyncRoutineNodeWidget extends AbstractRoutineNodeWidget {
                 maxDuration = (float) (Math.floor(maxDuration * 100f) / 100f);
                 microNodeView.setLabel(maxDuration + ""); // todo: use string builder here
             } else {
-                timelineWidget.clearMap();
                 microNodeView.clearMap();
                 microNodeView.setLabel("0.0");
 
@@ -377,7 +375,6 @@ public class AsyncRoutineNodeWidget extends AbstractRoutineNodeWidget {
         super.resetNode();
 
         microNodeView.clearMap();
-        timelineWidget.clearMap();
     }
 }
 
