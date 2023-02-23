929ae0baae86073b8fec8eb259b99b1089be0fdd
SenPie <smbat@duck.com>
2023-02-23 15:45:35 +0400

    project explorer should be opened with singleton asset to not have two instances

--- editor/src/com/talosvfx/talos/editor/project2/TalosControl.java
+++ editor/src/com/talosvfx/talos/editor/project2/TalosControl.java
@@ -175,7 +175,11 @@ public class TalosControl implements Observer {
         if(event.getPath().startsWith("window/apps/")) {
             Class<AppManager.BaseApp> clazz = (Class<AppManager.BaseApp>) event.getPayload();
 
-            SharedResources.appManager.openApp(AppManager.dummyAsset, clazz);
+            if (SharedResources.appManager.canOpenInApp(AppManager.singletonAsset, clazz)) {
+                SharedResources.appManager.openAppIfNotOpened(AppManager.singletonAsset, clazz);
+            } else {
+                SharedResources.appManager.openAppIfNotOpened(AppManager.dummyAsset, clazz);
+            }
 
             return;
         }
