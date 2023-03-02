5e273927df1a5b6d04bdeda425ded68ad0ce3be1
SenPie <smbat@duck.com>
2023-03-02 16:41:31 +0400

    Exectuor node is updated, when target scene is renamed.

--- editor/src/com/talosvfx/talos/editor/addons/scene/events/AssetPathChanged.java
+++ editor/src/com/talosvfx/talos/editor/addons/scene/events/AssetPathChanged.java
@@ -1,11 +1,12 @@
 package com.talosvfx.talos.editor.addons.scene.events;
 
+import com.badlogic.gdx.files.FileHandle;
 import com.talosvfx.talos.editor.notifications.TalosEvent;
 
 public class AssetPathChanged implements TalosEvent {
 
-    public String oldRelativePath;
-    public String newRelativePath;
+    public FileHandle oldHandle;
+    public FileHandle newHandle;
 
     @Override
     public void reset () {
